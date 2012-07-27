b631705f29b6a5d5d0477d4d46566ac4b9dcee5b
Alan Hollis <me@alanhollis.com>
2012-07-27 13:20:51 +0100

    Add my blog details

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -49,7 +49,7 @@
 	<hr />
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by <a href="http://alanhollis.com">http://alanhollis.com</a><br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
